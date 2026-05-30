6d22ec0770183131a584daf607823208b52c5e1a
Prashant-coder-dev <shresthaprashant253@gmail.com>
2026-05-30 11:54:42 +0545

    Add files via upload

--- shared/constants.py
+++ shared/constants.py
@@ -17,20 +17,8 @@ NEPALIPAISA_SUBINDEX_URL = "https://nepalipaisa.com/api/GetSubIndexLive"
 SHAREHUB_ANNOUNCEMENT_URL = "https://sharehubnepal.com/data/api/v1/announcement"
 SHAREHUB_OFFERING_URL = "https://sharehubnepal.com/data/api/v1/public-offering"
 
-GOOGLE_SHEET_CSV = (
-    "https://docs.google.com/spreadsheets/d/"
-    "1Q_En7VGGfifDmn5xuiF-t_02doPpwl4PLzxb4TBCW0Q"
-    "/export?format=csv"
-)
-
 CDSC_COMPANY_LIST_URL = "https://iporesult.cdsc.com.np/result/company/list"
 CDSC_CHECK_RESULT_URL = "https://iporesult.cdsc.com.np/result/company/check-result"
-
-# Technical Periods
-RSI_PERIOD = 14
-MA_PERIOD = 20
-MA_50 = 50
-MA_200 = 200
 
 # Common Headers
 DEFAULT_HEADERS = {
